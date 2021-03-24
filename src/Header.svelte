00f3d644b8df0e2cefbce6bddd1f2a745abdf9c2
Leonardo Prates <odran037@users.noreply.github.com>
2021-03-24 11:56:23 -0400

    Initial Commit

---
+++ src/Header.svelte
@@ -0,0 +1,26 @@
+<header>
+	<h1>Random Fact Generator</h1>
+</header>
+
+<style>
+	header {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto 15px auto;
+    border-bottom: 1px solid #ccc;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		header {
+			max-width: 75%;
+		}
+	}
+</style>
