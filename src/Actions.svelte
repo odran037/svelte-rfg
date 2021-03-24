60c67f08721fed2185213147a3e383184623749a
Leonardo Prates <odran037@users.noreply.github.com>
2021-03-24 13:07:09 -0400

    Fix file structure

---
+++ src/Actions.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let labels;
+	export let method;
+</script>
+
+{#each labels as label}
+		<button on:click={() => method(label)}>{label}</button>
+{/each}
---
+++ src/stores.js
@@ -0,0 +1,44 @@
+import { writable } from 'svelte/store';
+
+function createfavoritesList() {
+  const { subscribe, set, update } = writable([]);
+
+  const addFavorite = (newFav) => {
+    return update(favs => {
+      if (favs.includes(newFav)) {
+        return favs;
+      }
+
+      let favsList = [...favs, newFav];
+
+      localStorage.setItem('myFavoriteFacts', JSON.stringify(favsList));
+
+      return favsList;
+
+    });
+  }
+
+  const removeFavorite = (fav) => {
+    return update(favs => {
+      let favsList = favs.filter(currFav => currFav !== fav);
+
+      localStorage.setItem('myFavoriteFacts', JSON.stringify(favsList));
+
+      return favsList;
+    });
+  }
+
+  const loadFavorites = () => {
+    let favsList = JSON.parse(localStorage.getItem('myFavoriteFacts'));
+    set(favsList);
+  }
+
+  return {
+    subscribe,
+    add: (newFav) => addFavorite(newFav),
+    remove: (fav) => removeFavorite(fav),
+    load: () => loadFavorites()
+  };
+}
+
+export const favoritesList = createfavoritesList();
